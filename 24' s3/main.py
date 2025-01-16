45664679d75384bbd1fbae31588f81a08635e9e0
Ritesh Sivanathan <82885975+Ritesh-Sivanathan@users.noreply.github.com>
2025-01-16 14:02:55 -0500

    Update main.py

--- 24' s3/main.py
+++ 24' s3/main.py
@@ -1,4 +1,4 @@
-# https://cccgrader.com/getproblem.php?fid=1101463&authcode=59b56ad501cb989e0df1aac8abb2a0a0
+# https://cccgrader.com/getproblem.php
 # https://dmoj.ca/problem/ccc24s3
 
 # multiple swipes are valid (can swipe multiple times)
